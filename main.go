65fbf1ec35dcbca16e284b33ab4711382bfcfa97
skeffandor <skeffandor@gmail.com>
2025-11-06 21:46:06 +0300

    Implement CLI functionality

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/skeffandor/go-project-1/internal/cli"
+	"github.com/skeffandor/go-project-1/internal/manager"
+)
+
+func main() {
+	tm := manager.NewTaskManager()
+
+	if err := manager.Load(tm, "tm.json"); err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		if err := manager.Save(tm, "tm.json"); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	cli.InitCLI(tm)
 }
